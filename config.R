1500d0e499723a3b836288c95ee4732b738a141e
andzandz11 <andzandz11@users.noreply.github.com>
2023-12-19 14:27:45 -0500

    Update config.R

--- config.R
+++ config.R
@@ -1,23 +1,19 @@
-#disable scientific notation
-options(scipen = 999)
+#set paths  #don't forget the final /
+script_folder <- "/home/bio/Documents/refdat/"
+itsx_directory <- "/home/bio/Software/ITSx_1.1.3/" 
+vsearch_directory <- "C:/R/"
 
 #name of the run
 out_dir_name <- "output"
 
-#possible options SINTAX RDP
+#possible options: SINTAX RDP
 #any other value will just result in the metadata to be written (no sequence files)
 export_tax_style <- "SINTAX"
-
-#set directory to vsearch and ITSx executables
-#don't forget the final /
-vsearch_directory <- "C:/R/"
-
-itsx_directory <- "/home/bio/Software/ITSx_1.1.3/" 
 
 #define number of cores
 number_of_cores <- 10
 
-#This requires at least 20GB of free hard drive space.
+#If set to false requires at least 20GB of free hard drive space.
 delete_temporary_files <- FALSE
 
 #define search string to download data from GenBank
@@ -34,3 +30,5 @@ its_minlen <- 100  #both ITS1 and ITS2
 
 itsx_profiles <- "T,L" #Tracheophyta and Liverworts (needed for ferns). Change to anything else if other sequences are included (e.g. algae). See ITSx manual for codes.
 
+#disable scientific notation #do not change
+options(scipen = 999)
